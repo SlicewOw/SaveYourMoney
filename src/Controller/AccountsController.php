51a0eaab8160000a4fcab0e1a5d9c9df57127393
Sebastian Schulz <sebastian.schulz@plath.de>
2020-05-19 08:46:45 +0200

    Add accounts by form

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -7,6 +7,14 @@ use App\Entity\Account;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class AccountsController extends AbstractController
 {
     /**
@@ -21,6 +29,56 @@ class AccountsController extends AbstractController
             'controller_name' => 'AccountsController',
             'accounts' => $accounts
         ]);
+
+    }
+
+    /**
+     * @Route("/account/new"), name="new_account"
+     * @Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+
+        $account = new Account();
+
+        $form_builder = $this->createFormBuilder($account);
+        $form_builder->add(
+            'account_name',
+            TextType::class,
+            array(
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Name of the account (e.g. Giro)'
+                ),
+                'required' => true
+            )
+        );
+        $form_builder->add(
+            'save',
+            SubmitType::class,
+            array(
+                'label' => 'Save new account',
+                'attr' => array('class' => 'btn btn-success')
+            )
+        );
+
+        $form = $form_builder->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $account = $form->getData();
+            $entity_manager = $this->getDoctrine()->getManager();
+            $entity_manager->persist($account);
+            $entity_manager->flush();
+
+            return $this->redirectToRoute("accounts");
+
+        }
+
+        return $this->render('accounts/new_account.html.twig', array(
+            'form' => $form->createView()
+        ));
 
     }
 
