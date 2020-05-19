36db4e0fea810c1377cc6e0ce8e5c2aaba62b115
Sebastian Schulz <sebastian.schulz@plath.de>
2020-05-19 08:00:05 +0200

    Show accounts of database instead of hard-coded

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +14,13 @@ class AccountsController extends AbstractController
      */
     public function index()
     {
+
+        $accounts = $this->getDoctrine()->getRepository(Account::class)->findAll();
+
         return $this->render('accounts/index.html.twig', [
             'controller_name' => 'AccountsController',
+            'accounts' => $accounts
         ]);
+
     }
 }
